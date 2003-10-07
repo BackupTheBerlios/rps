8732f8a653924def9f8e5242ca2e238b6a96a4fd
thoma <thoma>
2003-10-07 08:48:02 +0000

    ""

---
+++ rps/src/rps.cc
@@ -0,0 +1,27 @@
+// generated 2003/10/1 13:35:03 CEST by thoma@Tiger.(none)
+// using glademm V2.0.0c_cvs
+//
+// newer (non customized) versions of this file go to rps.cc_new
+
+// This file is for your program, I won't touch it again!
+
+#include <config.h>
+#include <gtkmm/main.h>
+
+#include "main_window_RPS.hh"
+
+int main(int argc, char **argv)
+{  
+#if defined(ENABLE_NLS)
+   bindtextdomain (GETTEXT_PACKAGE, PACKAGE_LOCALE_DIR);
+   bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
+   textdomain (GETTEXT_PACKAGE);
+#endif //ENABLE_NLS
+   
+   Gtk::Main m(&argc, &argv);
+
+main_window_RPS *main_window_RPS = new class main_window_RPS();
+   m.run(*main_window_RPS);
+delete main_window_RPS;
+   return 0;
+}
