30a27b48996043371ef57c0b2058ca0e285f680d
thoma <thoma>
2003-10-21 08:36:08 +0000

    ""

--- rps/tools/cd_titel.cc
+++ rps/tools/cd_titel.cc
@@ -53,6 +53,7 @@ void remove_spaces(MS_t &MS)
    MR["/"]="_";
    MR["("]="_";
    MR[")"]="_";
+   MR["'"]="";
    for(MS_t::iterator i=MS.begin();i!=MS.end();++i)
     {
       for(std::map<std::string,std::string>::const_iterator j=MR.begin();j!=MR.end();++j)
