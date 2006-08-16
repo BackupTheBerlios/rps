6e1908eb2c3b67ebb0358718705f89e8d8f4dadc
thoma <thoma>
2006-08-16 13:00:53 +0000

    ""

--- pyrps/gtk_window.py
+++ pyrps/gtk_window.py
@@ -17,11 +17,6 @@ class MainWindow(gtk.Window):
 
     self.Play = MyPlay.MyPlay(path)
 
-    # init importend variables
-#    self.VecFiles = VecFiles 
-#    self.backend = PlotMaster(PLOTPROGRAM_NCVIEW)
-#    self.backend = PlotMaster(PLOTPROGRAM_GMT)
-
     # Create the toplevel window
     gtk.Window.__init__(self)   
     try:
@@ -42,6 +37,8 @@ class MainWindow(gtk.Window):
     self.Buttons = my_buttons.Buttons(self)
     main_vbox.pack_start(self.Buttons,expand=False,fill=True,padding=0)
 
+    self.show()
+    self.get_screen().get_display().flush()    
 
     paned = gtk.HPaned()
     main_vbox.pack_start(paned)
@@ -51,24 +48,6 @@ class MainWindow(gtk.Window):
 
     self.playlist = gtk.Frame("Playlist")
     paned.pack2(self.playlist, resize=False, shrink=True)
-
-#    main_vbox.pack_start(self.filetree,expand=True,fill=True,padding=0)
-    
-
-#    self.notebook = gtk.Notebook()
-#    main_vbox.pack_start(self.notebook,expand=True,fill=True,padding=0)
-#    __nvb = gtk.VBox()
-#    self.notebook.append_page(__nvb, tab_label=gtk.Label("Main Page"))
-
-#    self.Trees = my_trees.Trees(self)
-#    __nvb.pack_start(self.Trees,expand=True,fill=True,padding=0)
-
-#    self.notebook.append_page(self.plot_output_holder(), tab_label=self.plot_output_
-
-
-#    self.option_box = my_options.Options(self)
-#    __nvb.pack_start(self.option_box,expand=False,fill=True,padding=0)
-
 
     self.show_all()
 ########################################################
